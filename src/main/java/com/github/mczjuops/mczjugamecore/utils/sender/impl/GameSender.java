7e1141fe8fad6ca249fd368774fb811ca0c81ede
Yikuni <yikuni@qq.com>
2026-05-03 18:19:27 +0800

    为游戏内的所有玩家广播消息

---
+++ src/main/java/com/github/mczjuops/mczjugamecore/utils/sender/impl/GameSender.java
@@ -0,0 +1,47 @@
+package com.github.mczjuops.mczjugamecore.utils.sender.impl;
+
+import com.github.mczjuops.mczjugamecore.game.AbstractGame;
+import com.github.mczjuops.mczjugamecore.player.PlayerExt;
+import com.github.mczjuops.mczjugamecore.player.party.Party;
+import com.github.mczjuops.mczjugamecore.utils.TextParser;
+import com.github.mczjuops.mczjugamecore.utils.sender.Sender;
+
+public class GameSender implements Sender {
+
+    private final AbstractGame game;
+
+    public GameSender(AbstractGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void info(String msg) {
+        send(msg, "reset");
+    }
+
+    @Override
+    public void warn(String msg) {
+        send(msg, "yellow");
+    }
+
+    @Override
+    public void error(String msg) {
+        send(msg, "red");
+    }
+
+    @Override
+    public void success(String msg) {
+        send(msg, "green");
+    }
+
+    @Override
+    public void primary(String msg) {
+        send(msg, "blue");
+    }
+
+    private void send(String msg, String color){
+        for (PlayerExt player : game.getPlayers()) {
+            player.player().sendMessage(TextParser.parse(STR."<red>[<white>\{game.getName()}</white>]: <\{color}>\{msg}"));
+        }
+    }
+}
