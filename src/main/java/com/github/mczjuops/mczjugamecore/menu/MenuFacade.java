2bb59e8578041146672aee24f323373fa12ec1ff
tozero <74749129+tozero0@users.noreply.github.com>
2026-05-07 14:46:07 +0800

    修复点击自己物品栏区域时误判为菜单按钮的问题

--- src/main/java/com/github/mczjuops/mczjugamecore/menu/MenuFacade.java
+++ src/main/java/com/github/mczjuops/mczjugamecore/menu/MenuFacade.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.Range;
 
 import java.util.HashMap;
@@ -52,9 +53,13 @@ public class MenuFacade implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent event){
-        if (!(event.getView().getTopInventory().getHolder() instanceof Menu menu)) return;
+        Inventory top = event.getView().getTopInventory();
+        if (!(top.getHolder() instanceof Menu menu)) return;
+
         event.setCancelled(true);
-        menu.handleClick(event);
+
+        int slot = event.getRawSlot();
+        if (slot >= 0 && slot < top.getSize()) menu.handleClick(event);
     }
 
     @EventHandler
